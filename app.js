e92c2c11d0a5195b2eccc7091e2c41244ec15bd1
Kevin Toledo <kgtoledoc@unal.edu.co>
2018-10-15 18:05:32 -0500

    Changes in layout

--- app.js
+++ app.js
@@ -6,18 +6,16 @@ port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+app.get('/profile', (req, res) => {
   let user = {
     name: 'Joe',
     img: 'http://imagenpng.com/wp-content/uploads/2015/08/los-10-mejores-personajes-de-caricaturas8.jpg',
-    friends: [
-      'Andres',
-      'Sexy',
-      'Sal',
-      'Aja',
-    ]
   }
-  res.render('index', {user: user});
+  res.render('profile', {user: user});
+})
+
+app.get('/', (req, res) => {
+  res.render('home')
 })
 
 app.listen(port, (err) => {
